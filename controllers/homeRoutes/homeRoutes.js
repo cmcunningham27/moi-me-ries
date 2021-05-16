f7df22a0adc3f324a0dc6d117e010473f80cdd1b
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-16 14:57:05 -0700

    changed background and layout of home and login page main section

--- controllers/homeRoutes/homeRoutes.js
+++ controllers/homeRoutes/homeRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require('../../utils/auth');
 router.get('/', async (req, res) => {
     try {
         res.render('homepage', {
-            logged_in: req.session.logged_in
+            // logged_in: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err);
